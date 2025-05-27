9764a45234135ccc8b5299a8babb0df730b82719
viettr <viettran295@gmail.com>
2025-05-27 14:17:11 +0200

    Refactor processing enum

--- src/handler/mod.rs
+++ src/handler/mod.rs
@@ -2,7 +2,7 @@ use actix_web::{web::{self, Query}, HttpResponse};
 use log::error;
 use serde::Deserialize;
 
-use crate::{scanner::Backtest, fetch::StockFetcher};
+use crate::{fetch::StockFetcher, scanner::{Backtest, ScannerPerformance, ScannerCrossingMA}};
 use crate::processor::{Strategy, StrategyCrossingMA, StrategyRSI};
 use crate::converter::DfConverter;
 
@@ -18,6 +18,11 @@ pub struct QueryParams {
     end_date: Option<String>,
     short_ma: Option<usize>,
     long_ma: Option<usize>
+}
+
+enum ProcessStrategy {
+    ResultStrategy(Box<dyn FnOnce(DfConverter, &Query<QueryParams>) -> Result<String, Box<dyn std::error::Error>>>),
+    OptionStrategy(Box<dyn FnOnce(&Query<QueryParams>) -> Option<(&String, &f32)>>)
 }
 
 pub async fn get_price(
@@ -42,14 +47,11 @@ pub async fn get_price(
     }
 }
 
-async fn fetch_and_process<F>(
+async fn fetch_and_process(
     symbol: web::Path<String>,
     query: &Query<QueryParams>,
-    process_fn: F
-) -> HttpResponse 
-where 
-    F: FnOnce(DfConverter, &Query<QueryParams>) -> Result<String, Box<dyn std::error::Error>>,
-{
+    process_fn: ProcessStrategy
+) -> HttpResponse {
     let fetcher = StockFetcher::new();
     let start_date = query.start_date.clone();
     let end_date = query.end_date.clone();
@@ -59,16 +61,24 @@ where
             let mut df_proc = DfConverter::new();
             match df_proc.to_df(stock_data) {
                 Ok(_) => {
-                    match process_fn(df_proc, query) {
-                        Ok(response) => {
-                            return HttpResponse::Ok()
-                                .content_type("application/json")
-                                .body(response);
-                        },
-                        Err(e) => {
-                            error!("Error calculating signal: {}", e);
+                    match process_fn{
+                        ProcessStrategy::ResultStrategy(resutl_fn) => {
+                            match resutl_fn(df_proc, query) {
+                                Ok(response) => {
+                                    return HttpResponse::Ok()
+                                        .content_type("application/json")
+                                        .body(response);
+                                },
+                                Err(e) => {
+                                    error!("Error calculating signal: {}", e);
+                                    return HttpResponse::InternalServerError()
+                                        .body(format!("Error calculating signal: {}", e))
+                                }
+                            }
+                        }
+                        _ => {
                             return HttpResponse::InternalServerError()
-                                .body(format!("Error calculating signal: {}", e))
+                                .body(format!("Error processing data"))
                         }
                     }
                 },
@@ -87,38 +97,41 @@ where
     }
 }
 
-pub async fn get_ma_signal(
+async fn get_ma_signal(
     symbol: web::Path<String>,
     ma_type: &str,
     query: Query<QueryParams>
 ) -> HttpResponse {
-    fetch_and_process(symbol, &query, |df_proc, query| {
-        let short_ma = query.short_ma.clone().unwrap_or(20);
-        let long_ma = query.long_ma.clone().unwrap_or(50);
-        let mut crs_avg = StrategyCrossingMA::new(
-                                    df_proc.df.unwrap(), 
-                                    short_ma, 
-                                    long_ma, 
-                                    ma_type.to_string()
-                            );
-        match crs_avg.calc_signal() {
-            Ok(_) => {
-                let mut bt = Backtest::new();
-                if let Some(ref df) = crs_avg.df {
-                    bt.execute(df, "Sig_SMA_20_50"); 
+    let ma_type_owned = ma_type.to_string();
+    fetch_and_process(
+        symbol, 
+        &query, 
+        ProcessStrategy::ResultStrategy(
+            Box::new(|df_proc, query| {
+                let short_ma = query.short_ma.clone().unwrap_or(20);
+                let long_ma = query.long_ma.clone().unwrap_or(50);
+                let mut crs_avg = StrategyCrossingMA::new(
+                                            df_proc.df.unwrap(), 
+                                            short_ma, 
+                                            long_ma, 
+                                            ma_type_owned
+                                    );
+                match crs_avg.calc_signal() {
+                    Ok(_) => {
+                        let response = DfConverter::crossingma_df_to_json(&crs_avg.df.clone().unwrap());
+                        Ok(response)
+                    }
+                    Err(e) => {
+                        error!("Error calculating signal: {}", e);
+                        Err(Box::new(std::io::Error::new(
+                            std::io::ErrorKind::Other,
+                            format!("Error calculating signal: {}", e)
+                        )))
+                    }
                 }
-                let response = DfConverter::crossingma_df_to_json(&crs_avg.df.clone().unwrap());
-                Ok(response)
-            }
-            Err(e) => {
-                error!("Error calculating signal: {}", e);
-                Err(Box::new(std::io::Error::new(
-                    std::io::ErrorKind::Other,
-                    format!("Error calculating signal: {}", e)
-                )))
-            }
-        }
-    }).await
+            })
+        )
+    ).await
 }
 
 pub async fn get_sma_signal(
@@ -139,21 +152,27 @@ pub async fn get_rsi_signal(
     symbol: web::Path<String>,
     query: Query<QueryParams>
     ) -> HttpResponse {
-    fetch_and_process(symbol, &query, |df_proc, _| {
-        let mut rsi_str = StrategyRSI::new(df_proc.df.unwrap(), 14, 80, 20);
-        match rsi_str.calc_rsi() {
-            Ok(_) => {
-                rsi_str.calc_signal()?;
-                let response = DfConverter::rsi_df_to_json(&rsi_str.df.clone().unwrap());
-                return Ok(response);
-            }
-            Err(e) => {
-                error!("Error calculating RSI stock prices: {}", e);
-                return Err(Box::new(std::io::Error::new(
-                            std::io::ErrorKind::Other,
-                            format!("Error calculating signal: {}", e)
-                        )));
-            }
-        }
-    }).await
+    fetch_and_process(
+        symbol, 
+        &query, 
+        ProcessStrategy::ResultStrategy(
+            Box::new(|df_proc, _| {
+                let mut rsi_str = StrategyRSI::new(df_proc.df.unwrap(), 14, 80, 20);
+                match rsi_str.calc_rsi() {
+                    Ok(_) => {
+                        rsi_str.calc_signal()?;
+                        let response = DfConverter::rsi_df_to_json(&rsi_str.df.clone().unwrap());
+                        return Ok(response);
+                    }
+                    Err(e) => {
+                        error!("Error calculating RSI stock prices: {}", e);
+                        return Err(Box::new(std::io::Error::new(
+                                    std::io::ErrorKind::Other,
+                                    format!("Error calculating signal: {}", e)
+                                )));
+                    }
+                }
+            })
+        )
+    ).await
 }
