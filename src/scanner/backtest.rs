f74b6dd865a4bb81325693940ef7aa7b2c7fb125
viettr <viettran295@gmail.com>
2025-05-29 11:48:16 +0200

    Add shares hold in backtest

--- src/scanner/backtest.rs
+++ src/scanner/backtest.rs
@@ -5,6 +5,7 @@ use std::collections::HashMap;
 #[derive(Debug)]
 pub struct Backtest{
     pub col_name: String,
+    pub shares_hold: i32,
     pub results: HashMap<String, f32>
 }
 
@@ -12,9 +13,11 @@ impl Backtest {
     pub fn new() -> Self {
         Backtest {
             col_name: String::from("Backtest"),
+            shares_hold: 0,
             results: HashMap::new()
         }
     }
+
     pub fn execute(&mut self, df: &DataFrame, sig_col: &str) {
         let df_bt = df.clone()
             .lazy()
@@ -23,37 +26,30 @@ impl Backtest {
             )
             .collect().ok();
         if let Some(df_bt) = df_bt{
-            // let sum = df_bt.column("backtest").unwrap().f64().unwrap().sum().unwrap();
             let mut cum_sum = 0.0;
-            let mut trades = 0;
-            let mut last_exec_price = 0.0;
-            let mut first_trade: bool = true;
+            self.shares_hold = 0;
             for i in (0..df_bt.height()).rev() {
-                if let Some(exec_price) = self.get_exec_price(df_bt.column("Backtest").unwrap().get(i).unwrap()) {
-                    if exec_price > 0.0 && first_trade {
-                        first_trade = false;
-                        continue;
-                    }
-                    if exec_price != 0.0 {
+                if let Some(exec_price) = self.anyvalue_to_float(df_bt.column("Backtest").unwrap().get(i).unwrap()) {
+                    let share = self.anyvalue_to_float(df_bt.column(sig_col).unwrap().get(i).unwrap()).unwrap();
+                    if ((exec_price < 0.0) & (self.shares_hold == 0) ) ||
+                        ((exec_price != 0.0) & (self.shares_hold > 0)) {
                         cum_sum += exec_price;
-                        trades += 1;
-                        last_exec_price = exec_price;
+                        self.shares_hold -= share as i32;
                     }
                 } else {
                     debug!("Unsupported value type in backtest column at row {}", i);
                 }
             }
-            // Trading pair must be even
-            if trades % 2 != 0 {
-                cum_sum -= last_exec_price;
-            }
             self.results.insert(sig_col.to_string(), cum_sum);
         }
     }
-    fn get_exec_price(&self, av: AnyValue) -> Option<f32> {
+
+    fn anyvalue_to_float(&self, av: AnyValue) -> Option<f32> {
         match av {
             AnyValue::Float64(bt_value) => Some(bt_value as f32),
             AnyValue::Float32(bt_value) => Some(bt_value),
+            AnyValue::Int64(bt_value) => Some(bt_value as f32),
+            AnyValue::Int32(bt_value) => Some(bt_value as f32),
             _ => None
         }
     }
