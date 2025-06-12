0273d3c9c7e3f655c6c03b665b7ba20894098ca8
viettr <viettran295@gmail.com>
2025-06-12 22:15:59 +0200

    Add mutex for db manager

--- src/db/db.rs
+++ src/db/db.rs
@@ -1,13 +1,13 @@
 use indexmap::IndexMap;
 use polars::{frame::{row::AnyValueBuffer}, prelude::*};
 use duckdb::{Connection, Result};
-use std::{fs, path::Path};
-use log::error;
+use std::{fs, path::Path, sync::{Mutex, MutexGuard}};
+use log::{error, debug};
 
 #[derive(Debug)]
 pub struct DbManager {
     data_dir: String,
-    db_path: String
+    db_path: Arc<Mutex<String>>
 }
 
 impl Default for DbManager {
@@ -21,7 +21,7 @@ impl Default for DbManager {
         }
         DbManager {
             data_dir: data_dir.to_str().unwrap().to_string(),
-            db_path: String::from(db_path)
+            db_path: Arc::new(Mutex::new(String::from(db_path))) 
         }
     }
 }
@@ -30,11 +30,17 @@ impl DbManager {
     fn new(data_dir: String, db_path: String) -> Self {
         DbManager {
             data_dir: data_dir,
-            db_path: db_path
+            db_path: Arc::new(Mutex::new(db_path))
         }
     }
     
+    fn acquire_db(&self) -> anyhow::Result<MutexGuard<'_, String>> {
+        self.db_path.lock()
+            .map_err(|e| anyhow::anyhow!("Error acquiring db: {}", e))
+    }
+
     pub fn create_table(&self, table_name: String, df: &mut DataFrame) -> Result<()> {
+        let db_guard = self.acquire_db().unwrap();
         let parquet_file = format!("{}/{}.parquet", self.data_dir, table_name);
         let mut file = std::fs::File::create(parquet_file.clone()).expect("Failed to create file");
         ParquetWriter::new(&mut file)
@@ -42,18 +48,20 @@ impl DbManager {
             .finish(df)
             .expect("Error writing parquet file");
         
-        let conn = Connection::open(self.db_path.as_str())?;
+        let conn = Connection::open(db_guard.clone().as_str())?;
         let query = format!(
             "CREATE TABLE IF NOT EXISTS {} AS SELECT * FROM read_parquet('{}')", 
             table_name, 
             parquet_file
         );
         conn.execute(&query, [],)?;
+        debug!("Created {} table", table_name);
         Ok(())
     }
     
     pub fn table_exists(&self, table_name: String) -> Result<bool> {
-        let conn = Connection::open(self.db_path.as_str())?;
+        let db_guard = self.acquire_db().unwrap();
+        let conn = Connection::open(db_guard.clone().as_str())?;
         let query = format!(
             "SELECT EXISTS (
                 SELECT 1 FROM information_schema.tables
@@ -67,14 +75,16 @@ impl DbManager {
     }
     
     pub fn get_table(&self, table_name: String) -> Result<DataFrame> {
-        let conn = Connection::open(self.db_path.clone())?;
+        let mut series_vec: Vec<Series> = Vec::new();
+        let column_names_types = self.get_cols_names_types(table_name.clone())?;
+        let mut buffer: Vec<AnyValueBuffer> = self.create_buffer(column_names_types.values().cloned().collect());
+
+        let db_guard = self.acquire_db().unwrap();
+        let conn = Connection::open(db_guard.clone().as_str())?;
         let query = format!("SELECT * FROM {}", table_name);
         let mut stmt = conn.prepare(&query)?;
         let mut rows = stmt.query([])?;
 
-        let mut series_vec: Vec<Series> = Vec::new();
-        let column_names_types = self.get_cols_names_types(table_name.clone())?;
-        let mut buffer: Vec<AnyValueBuffer> = self.create_buffer(column_names_types.values().cloned().collect());
         while let Some(row) = rows.next()? {
             for (i, _) in column_names_types.iter().enumerate() {
                 let value: duckdb::types::Value = row.get(i)?;
@@ -94,10 +104,12 @@ impl DbManager {
         let df = DataFrame::new(series_vec
                                 .into_iter()
                                 .map(|s| s.into()).collect::<Vec<Column>>()).unwrap();
+        debug!("Queried table {}", table_name);
         Ok(df)
     }
 
     pub fn clean_up(&self) -> Result<()> {
+        let db_guard = self.acquire_db().unwrap();
         let entries = fs::read_dir(self.data_dir.as_str()).unwrap();
         for entry in entries {
             let path = entry.unwrap().path();
@@ -109,11 +121,13 @@ impl DbManager {
                 }
             }
         }
+        debug!("All data is clean");
         Ok(())
     }
 
     fn get_cols_names_types(&self, table_name: String) -> Result<IndexMap<String, String>> {
-        let conn = Connection::open(self.db_path.clone())?;
+        let db_guard = self.acquire_db().unwrap();
+        let conn = Connection::open(db_guard.clone().as_str())?;
         let query_cols_names = format!("PRAGMA table_info({})", table_name);
         let mut cols_name_stmt = conn.prepare(&query_cols_names)?;
         let rows = cols_name_stmt.query_map([], |row| {
